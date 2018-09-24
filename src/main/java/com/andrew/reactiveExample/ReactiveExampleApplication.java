5b0ceef652dc81d5f00fae86562d8b4b35aa04b6
exosyphon <exosyphon@yahoo.com>
2018-09-24 15:38:41 -0400

    initial commit

---
+++ src/main/java/com/andrew/reactiveExample/ReactiveExampleApplication.java
@@ -0,0 +1,12 @@
+package com.andrew.reactiveExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveExampleApplication.class, args);
+	}
+}
